b5667b7aaf3ffb99b70fbdd843f735db8c0848b9
Bart Salters <Bart@Barts-Mac.local>
2016-10-19 05:41:31 -0700

    Fixed airplane from to loc ;)

--- SQLiteCRUD/DetailViewController.swift
+++ SQLiteCRUD/DetailViewController.swift
@@ -14,13 +14,31 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
         @IBOutlet weak var mapView: MKMapView!
         @IBOutlet weak var nameLabel: UILabel!
         @IBOutlet weak var distanceLabel: UILabel!
+        var planeAnnotation: MKPointAnnotation!
+        var planeAnnotationPosition = 0
+    var polyline:MKGeodesicPolyline!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let schipholAirport = CLLocation(latitude: 52.3094593, longitude: 4.7600949)
         let currentAirport = CLLocation(latitude: (airport?.latitude)!, longitude: (airport?.longitude)!)
+        let distanceInMeters = schipholAirport.distance(from: currentAirport)
+        let regionRadius: CLLocationDistance = distanceInMeters
+
         nameLabel.text = airport?.name
         distanceLabel.text = String(schipholAirport.distance(from: currentAirport) / 1000) + "km";
+
+        func centerMapOnLocation(location: CLLocation){
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        
+        centerMapOnLocation(location: currentAirport)
+        
+        let avgLat = (schipholAirport.coordinate.latitude + currentAirport.coordinate.latitude)/2
+        let avglon = (schipholAirport.coordinate.longitude + currentAirport.coordinate.longitude)/2
+        
+        let avgcoor = CLLocationCoordinate2D(latitude: avgLat, longitude: avglon);
 
         
         // Add mappoints to Map
@@ -35,8 +53,9 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
         dropPin2.coordinate = currentAirportLocation
         dropPin2.title = "Amsterdam Schiphol"
         mapView.addAnnotation(dropPin2)
-        mapView.showAnnotations(self.mapView.annotations, animated: true)
+//        mapView.showAnnotations(self.mapView.annotations, animated: true)
         
+
         
         mapView.delegate = self
         
@@ -48,14 +67,49 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
             points.append(annotation.coordinate)
         }
         
+        mapView.setCenter(avgcoor, animated: true)
         
-        let polyline = MKGeodesicPolyline(coordinates: points, count: points.count)
+        polyline = MKGeodesicPolyline(coordinates: points, count: points.count)
         
         mapView.add(polyline)
         
-        mapView.camera.altitude *= 50.0;
+        let annotation = CustomPlaneAnnotation()
+        annotation.title = NSLocalizedString("Plane", comment: "Plane marker")
+        mapView.addAnnotation(annotation)
+        
+        self.planeAnnotation = annotation
+        self.updatePlanePosition()
         
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        let planeIdentifier = "Plane"
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: planeIdentifier) ?? MKAnnotationView(annotation: annotation, reuseIdentifier: planeIdentifier)
+        
+        if annotation is CustomPlaneAnnotation {
+            annotationView.image = UIImage.init(named: "rsz_airplane-2.png")
+
+            return annotationView
+        }
+        //self.view.addSubview(annotationView)
+        return nil
+    }
+    
+    func updatePlanePosition() {
+        let step = 25
+        guard planeAnnotationPosition + step < polyline.pointCount
+            else { return }
+        
+        let points = polyline.points()
+        self.planeAnnotationPosition += step
+        let nextMapPoint = points[planeAnnotationPosition]
+        
+        self.planeAnnotation.coordinate = MKCoordinateForMapPoint(nextMapPoint)
+        
+        perform(#selector(DetailViewController.updatePlanePosition), with: nil, afterDelay: 0.03)
+    }
+    
     
     //MARK:- MapViewDelegate methods
     
---
+++ SQLiteCRUD/PlaneAnnotation.swift
@@ -0,0 +1,20 @@
+//
+//  PlaneAnnotation.swift
+//  SQLiteCRUD
+//
+//  Created by Bart Salters on 19/10/2016.
+//  Copyright © 2016 Diederich Kroeske. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class CustomPlaneAnnotation : MKPointAnnotation
+{
+    
+}
+
+class CustomPlaneAnnotationView : MKAnnotationView
+{
+    
+}
